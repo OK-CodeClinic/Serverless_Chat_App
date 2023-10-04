9b730b8c652b718ebe9f2c8b29e0371c51fb25fe
Kehinde Omokungbe <kehindefranklin@gmail.com>
2023-10-04 16:01:43 +0100

    Updating Lamda function

--- js/config.js
+++ js/config.js
@@ -1,1 +1,3 @@
 var ChatApp = window.ChatApp || {};
+
+ChatApp.apiEndpoint = 'https://5nxkdulkdf.execute-api.eu-north-1.amazonaws.com/prod';
--- lambda_functions/lambda-chat-api.py
+++ lambda_functions/lambda-chat-api.py
@@ -1,34 +1,46 @@
 import json
 import boto3
 
+# Create an S3 client
 s3_client = boto3.client('s3')
 
-bucket_name = 'ken-serverless-chat-application'
+# Define the S3 bucket name
+bucket = 'ken-serverless-chat-application'
 
 def lambda_handler(event, context):
+    # Extract the path from the event
+    path = event['pathParameters']['proxy']
+
+    key = None
+
+    if path == 'conversations':
+        key = 'data/conversations.json'
+    elif path.startswith('conversations/'):
+        id = path[len('conversations/'):]
+        key = f'data/conversations/{id}.json'
+    else:
+        return done('No cases hit')
+
     try:
         # Retrieve the object from S3
-        response = s3_client.get_object(Bucket=bucket_name, Key='data/conversations.json')
-        
-        # Read the response body (assuming it's JSON) and parse it
-        response_body = response['Body'].read().decode('utf-8')
-        parsed_response = json.loads(response_body)
-        
-        return {
-            'statusCode': 200,
-            'body': json.dumps(parsed_response),
-            'headers': {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*'
-            }
-        }
+        response = s3_client.get_object(Bucket=bucket, Key=key)
+        body = response['Body'].read().decode('utf-8')
+        return done(None, json.loads(body))
     except Exception as e:
-        print(e)
-        return {
-            'statusCode': 400,
-            'body': json.dumps(str(e)),
-            'headers': {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*'
-            }
+        return done(str(e))
+
+def done(err, res=None):
+    if err:
+        print(err)
+
+    status_code = '400' if err else '200'
+    body = json.dumps(err) if err else json.dumps(res)
+
+    return {
+        'statusCode': status_code,
+        'body': body,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': 'http://ken-serverless-chat-application.s3-website.us-east-2.amazonaws.com'
         }
+    }
