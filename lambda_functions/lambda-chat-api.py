13532eed72b0ef00e1e3018804a5cc6188e0f840
Kehinde Omokungbe <kehindefranklin@gmail.com>
2023-10-05 16:11:56 +0100

    Updating my lambda function and js

--- lambda_functions/lambda-chat-api.py
+++ lambda_functions/lambda-chat-api.py
@@ -1,33 +1,125 @@
 import json
 import boto3
+from botocore.exceptions import ClientError
 
-# Create an S3 client
-s3_client = boto3.client('s3')
-
-# Define the S3 bucket name
-bucket = 'ken-serverless-chat-application'
+# Create DynamoDB client
+dynamodb = boto3.client('dynamodb')
 
 def lambda_handler(event, context):
-    # Extract the path from the event
     path = event['pathParameters']['proxy']
 
-    key = None
-
-    if path == 'conversations':
-        key = 'data/conversations.json'
-    elif path.startswith('conversations/'):
-        id = path[len('conversations/'):]
-        key = f'data/conversations/{id}.json'
-    else:
-        return done('No cases hit')
-
     try:
-        # Retrieve the object from S3
-        response = s3_client.get_object(Bucket=bucket, Key=key)
-        body = response['Body'].read().decode('utf-8')
-        return done(None, json.loads(body))
-    except Exception as e:
+        if path == 'conversations':
+            conversation_data = fetch_conversations()
+            return done(None, conversation_data)
+        elif path.startswith('conversations/'):
+            id = path[len('conversations/'):]
+            conversation_data = fetch_conversation_data(id)
+            return done(None, conversation_data)
+        else:
+            return done('No cases hit')
+    except ClientError as e:
         return done(str(e))
+
+def fetch_conversations():
+    try:
+        response = dynamodb.query(
+            TableName='Chat-Conversations',
+            IndexName='Username-ConversationId-index',
+            Select='ALL_PROJECTED_ATTRIBUTES',
+            KeyConditionExpression='Username = :username',
+            ExpressionAttributeValues={':username': {'S': 'Student'}}
+        )
+        conversation_ids = [item['ConversationId']['S'] for item in response.get('Items', [])]
+
+        # Fetch last messages and participants for each conversation
+        last_messages = fetch_last_messages(conversation_ids)
+        participants = fetch_participants(conversation_ids)
+
+        # Prepare the result objects
+        result_objs = []
+        for id in conversation_ids:
+            result_objs.append({
+                'id': id,
+                'last': last_messages.get(id),
+                'participants': participants.get(id, [])
+            })
+
+        return result_objs
+    except ClientError as e:
+        return done(str(e))
+
+def fetch_last_messages(conversation_ids):
+    last_messages = {}
+    for id in conversation_ids:
+        response = dynamodb.query(
+            TableName='Chat-Messages',
+            ProjectionExpression='ConversationId, #T',
+            Limit=1,
+            ScanIndexForward=False,
+            KeyConditionExpression='ConversationId = :id',
+            ExpressionAttributeNames={'#T': 'Timestamp'},
+            ExpressionAttributeValues={':id': {'S': id}}
+        )
+        if response.get('Items'):
+            last_messages[id] = int(response['Items'][0]['Timestamp']['N'])
+    return last_messages
+
+def fetch_participants(conversation_ids):
+    participants = {}
+    for id in conversation_ids:
+        response = dynamodb.query(
+            TableName='Chat-Conversations',
+            Select='ALL_ATTRIBUTES',
+            KeyConditionExpression='ConversationId = :id',
+            ExpressionAttributeValues={':id': {'S': id}}
+        )
+        participants[id] = [item['Username']['S'] for item in response.get('Items', [])]
+    return participants
+
+def fetch_conversation_data(id):
+    messages = fetch_messages(id)
+    conversation_detail = fetch_conversation_detail(id, messages)
+    return {
+        'id': id,
+        'participants': conversation_detail['participants'],
+        'last': messages[-1]['time'] if messages else None,
+        'messages': messages
+    }
+
+def fetch_messages(id):
+    messages = []
+    response = dynamodb.query(
+        TableName='Chat-Messages',
+        ProjectionExpression='#T, Sender, Message',
+        ExpressionAttributeNames={'#T': 'Timestamp'},
+        KeyConditionExpression='ConversationId = :id',
+        ExpressionAttributeValues={':id': {'S': id}}
+    )
+    for item in response.get('Items', []):
+        messages.append({
+            'sender': item['Sender']['S'],
+            'time': int(item['Timestamp']['N']),
+            'message': item['Message']['S']
+        })
+    return messages
+
+def fetch_conversation_detail(id, messages):
+    participants = []
+    response = dynamodb.query(
+        TableName='Chat-Conversations',
+        Select='ALL_ATTRIBUTES',
+        KeyConditionExpression='ConversationId = :id',
+        ExpressionAttributeValues={':id': {'S': id}}
+    )
+    for item in response.get('Items', []):
+        participants.append(item['Username']['S'])
+    return {
+        'id': id,
+        'participants': participants,
+        'last': messages[-1]['time'] if messages else None,
+        'messages': messages
+    }
 
 def done(err, res=None):
     if err:
@@ -41,6 +133,6 @@ def done(err, res=None):
         'body': body,
         'headers': {
             'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': 'http://ken-serverless-chat-application.s3-website.us-east-2.amazonaws.com'
+            'Access-Control-Allow-Origin': '*'
         }
     }
