acdf5b95d7bbdcb7af76c4f48ca737dbb69fc8de
Kehinde Omokungbe <kehindefranklin@gmail.com>
2023-10-06 11:58:49 +0100

    Dividing my lambda functions from monolitic

---
+++ lambda_functions/chat-conversations-GET.py
@@ -0,0 +1,66 @@
+import boto3
+
+dynamodb = boto3.client('dynamodb')
+
+def lambda_handler(event, context):
+
+    conversation_ids = []
+    paginator = dynamodb.get_paginator('query')
+    for page in paginator.paginate(
+        TableName='Chat-Conversations',
+        IndexName='Username-ConversationId-index',
+        Select='ALL_PROJECTED_ATTRIBUTES',
+        KeyConditionExpression='Username = :username',
+        ExpressionAttributeValues={':username': {'S': 'Student'}}
+    ):
+        for item in page['Items']:
+            conversation_ids.append(item['ConversationId']['S'])
+
+    # Load last messages
+    lasts = load_convos_last(conversation_ids)
+
+    # Load conversation participants
+    parts = load_convo_participants(conversation_ids)
+
+    # Construct the response
+    response = []
+    for id in conversation_ids:
+        response.append({
+            'id': id,
+            'last': lasts[id],
+            'participants': parts[id]
+        })
+
+    return response
+
+def load_convos_last(ids):
+    result = {}
+    for id in ids:
+        response = dynamodb.query(
+            TableName='Chat-Messages',
+            ProjectionExpression='ConversationId, #T',
+            Limit=1,
+            ScanIndexForward=False,
+            KeyConditionExpression='ConversationId = :id',
+            ExpressionAttributeNames={'#T': 'Timestamp'},
+            ExpressionAttributeValues={':id': {'S': id}}
+        )
+        if len(response['Items']) == 1:
+            result[id] = int(response['Items'][0]['Timestamp']['N'])
+    return result
+
+def load_convo_participants(ids):
+    result = {}
+    for id in ids:
+        participants = []
+        paginator = dynamodb.get_paginator('query')
+        for page in paginator.paginate(
+            TableName='Chat-Conversations',
+            Select='ALL_ATTRIBUTES',
+            KeyConditionExpression='ConversationId = :id',
+            ExpressionAttributeValues={':id': {'S': id}}
+        ):
+            for item in page['Items']:
+                participants.append(item['Username']['S'])
+        result[id] = participants
+    return result
---
+++ lambda_functions/chat-message-POST.py
@@ -0,0 +1,26 @@
+import boto3
+import time
+
+dynamodb = boto3.client('dynamodb')
+
+def lambda_handler(event, context):
+    # Extract values from the event object
+    conversation_id = event['id']
+    message = event['message']
+
+    # Create a timestamp in milliseconds
+    timestamp = str(int(time.time() * 1000))
+
+    # Define the item to be inserted into the DynamoDB table
+    item = {
+        'ConversationId': {'S': conversation_id},
+        'Timestamp': {'N': timestamp},
+        'Message': {'S': message},
+        'Sender': {'S': 'Student'}
+    }
+
+    # Insert the item into the DynamoDB table
+    dynamodb.put_item(
+        TableName='Chat-Messages',
+        Item=item
+    )
